69547d1ebf80788a19c5ceb05fbc4bf47f0f04a3
Alice Liang <alice@climate.com>
2014-02-16 14:34:08 -0800

    initial commit

---
+++ init.el
@@ -0,0 +1,36 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+(add-hook 'clojure-mode-hook 'paredit-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(font-use-system-font t)
+ '(show-paren-mode t)
+ '(text-mode-hook (quote (text-mode-hook-identify)))
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil)
+ '(transient-mark-mode nil)
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
+(add-hook 'nrepl-interaction-mode-hook
+  'nrepl-turn-on-eldoc-mode)
+(setq nrepl-popup-stacktraces nil)
+(add-hook 'nrepl-mode-hook 'paredit-mode)
+
+(add-to-list 'load-path "~/.emacs.d/jdee-2.4.1/lisp")
+(load "jde")
+
+(global-set-key (kbd "C-x <up>") 'windmove-up)
+(global-set-key (kbd "C-x <down>") 'windmove-down)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+(global-set-key (kbd "C-x <left>") 'windmove-left)
---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject lisp-brunch "dynamic-version"
+  :description
+    "clojurescript/clojure project!"
+  :min-lein-version "2.0.0"
+  :url
+    "https://github.com/aliceliang/lisp-brunch"
+  :source-paths ["src/clj"]
+  :resource-paths ["resources"]
+  :checksum :warn
+  :dependencies
+    [[org.clojure/clojure "1.5.1"]]
+  :exclusions [org.clojure/clojure])
